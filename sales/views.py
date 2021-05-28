951c6bc1d8deb517490f9ad22bc89154eb59f7a9
Anisha Shruti A.T <anishashruti.at@gmail.com>
2021-05-28 22:55:47 +0530

    added templates

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Sale
 # Create your views here.
+#function view are readable than class based views
+def home_view(request):
+    st='hello this is anisha'
+    return render(request,'sales/home.html',{'st':st})
+
+class SalesListView(ListView):
+    model = Sale
+    template_name = 'sales/main.html'
